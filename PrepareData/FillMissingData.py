2952d7771fabcec69dfc93ff6026d613040c3130
NgocThach <19520265@gm.uit.edu.vn>
2022-05-07 14:24:58 +0700

    Done RT MLR

--- PrepareData/FillMissingData.py
+++ PrepareData/FillMissingData.py
@@ -1,7 +1,9 @@
+from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 
 data = pd.read_csv("House_Dataset.csv")
+print(data)
 df = pd.DataFrame(data)
 df = df.dropna(how = 'all')
 df['Số toilet'] = df['Số toilet'].replace('None', -1)
@@ -9,6 +11,7 @@ df['Số tầng'] = df['Số tầng'].replace('None', -1)
 df['Số phòng ngủ'] = df['Số phòng ngủ'].replace('None', -1)
 df['Diện Tích'] = df['Diện Tích'].astype(str)
 df['Giá'] = df['Giá'].astype(str)
+
 
 df = df.drop('Hướng ban công', 1)
 df = df.drop('Hướng nhà', 1)
@@ -19,6 +22,32 @@ df = df.drop('Địa chỉ', 1)
 
 df.drop_duplicates()
 df.dropna()
+# print(df)
+
+# dff = data.to_numpy()
+# xprice = dff[ :, 0]#Areage
+# yareage = dff[ :, 1]#Price
+# ybedroom = dff[ :, 6]#Bedroom
+# ytoilet = dff[ :, 7]#toilet
+# yfloor = dff[ :, 8]#floor
+
+# plt.figure()
+# plt.plot(xprice, yareage, 'o')
+# plt.xlabel("Price")
+# plt.ylabel("Areage")
+# plt.figure()
+# plt.xlabel("Price")
+# plt.ylabel("Bedrooms")
+# plt.plot(xprice, ybedroom, 'o' )
+# plt.figure()
+# plt.xlabel("Price")
+# plt.ylabel("Toilets")
+# plt.plot(xprice, ytoilet, 'o')
+# plt.figure()
+# plt.xlabel("Price")
+# plt.ylabel("Floors")
+# plt.plot(xprice, yfloor, 'o')
+# plt.show()
 
 # xoa het none
 df.drop(df[df['Số toilet'].astype(int) == -1].index, inplace = True)
@@ -31,8 +60,10 @@ df.drop(df[df['Số tầng'].astype(int) >= 10].index, inplace = True)
 df.drop(df[df['Số phòng ngủ'].astype(int) >= 15].index, inplace = True)
 df.drop(df[df['Diện Tích'].astype(str) == ("Địa" or "None")].index, inplace = True)
 df.drop(df[df['Giá'].astype(str) == ("Loại" or "None")].index, inplace = True)
-df['Diện Tích'] = df['Diện Tích'].astype(float)
 df['Giá'] = df['Giá'].astype(float)
+df['Diện Tích'] = df['Diện Tích'].astype(float)
+df.drop(df[df['Giá'].astype(float) >= 90].index, inplace = True)
+df.drop(df[df['Diện Tích'].astype(float) >= 300].index, inplace = True)
 df = df.reset_index(drop=True)
 
 ctoilet = df['Số toilet'].value_counts()
--- Regression/Regression.py
+++ Regression/Regression.py
@@ -1,3 +1,4 @@
+from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
@@ -37,7 +38,7 @@ print("Multiple Linear Regression Model Score is ",round(mlr.score(X_test,y_test
 print("Decision tree Regression Model Score is:",round(tr_regressor.score(X_test,y_test)*100))
 
 models_score =pd.DataFrame({'Model':['Multiple Linear Regression','Decision Tree'],
-                            'Score':[mlr_score,decision_score],
+                            'Score':[mlr_score * 100,decision_score * 100],
                             'Explained Variance Score':[expl_mlr,expl_tr]
                            })
 models_score.sort_values(by='Score',ascending=False)
@@ -48,10 +49,62 @@ New_Data = [[[100,1,2,2]]
            ,[[100,2,2,1]]
            ,[[100,2,2,2]]]
 
+TRR_Result = []
 print("Regression Tree Predict: ")
 for index, it in enumerate(New_Data):
     print("Predict " + str(index) + ": " + str(tr_regressor.predict(it)))
+    TRR_Result.append(tr_regressor.predict(it))
 
+MLR_Result = []
 print("Multiple Linear Regression Predict: ")
 for index, it in enumerate(New_Data):
     print("Predict " + str(index) + ": " + str(mlr.predict(it)))
+    MLR_Result.append(mlr.predict(it))
+
+df = dataset.to_numpy()
+xprice = df[ :, 0]#Areage
+yareage = df[ :, 1]#Price
+ybedroom = df[ :, 2]#Bedroom
+ytoilet = df[ :, 3]#toilet
+yfloor = df[ :, 4]#floor
+
+plt.figure()
+plt.plot(xprice, yareage, 'o')
+plt.xlabel("Price")
+plt.ylabel("Areage")
+for index, it in enumerate(MLR_Result):
+    plt.plot(it, New_Data[index][0][0], 'o', color = 'red')
+for index, it in enumerate(TRR_Result):
+    plt.plot(it, New_Data[index][0][0], 'o', color = 'yellow')
+plt.figure()
+
+plt.xlabel("Price")
+plt.ylabel("Bedrooms")
+plt.plot(xprice, ybedroom, 'o' )
+for index, it in enumerate(MLR_Result):
+    plt.plot(it, New_Data[index][0][1], 'o', color = 'red')
+for index, it in enumerate(TRR_Result):
+    plt.plot(it, New_Data[index][0][1], 'o', color = 'yellow')
+plt.figure()
+
+
+plt.xlabel("Price")
+plt.ylabel("Toilets")
+plt.plot(xprice, ytoilet, 'o')
+plt.figure()
+for index, it in enumerate(MLR_Result):
+    plt.plot(it, New_Data[index][0][2], 'o', color = 'red')
+for index, it in enumerate(TRR_Result):
+    plt.plot(it, New_Data[index][0][2], 'o', color = 'yellow')
+
+
+plt.xlabel("Price")
+plt.ylabel("Floors")
+plt.plot(xprice, yfloor, 'o')
+for index, it in enumerate(MLR_Result):
+    plt.plot(it, New_Data[index][0][3], 'o', color = 'red')
+for index, it in enumerate(TRR_Result):
+    plt.plot(it, New_Data[index][0][3], 'o', color = 'yellow')
+
+
+plt.show()
